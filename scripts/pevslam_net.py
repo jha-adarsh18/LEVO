f27869895d7c565b91d445044db8e0bc4dda5d21
Adarsh Jha <jhaadarsh350@gmail.com>
2025-10-05 19:08:20 +0530

    updated first module training script

--- scripts/pevslam_net.py
+++ scripts/pevslam_net.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from eventcloud import HierarchyStructure
+from scripts.eventcloud import HierarchyStructure
 
 class FeaturePropagation(nn.Module):
     """
